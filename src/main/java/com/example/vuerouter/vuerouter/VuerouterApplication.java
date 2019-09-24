ad472e66bbf76f9c9e55b484e4daf3cf902b5595
albert0715 <sjtv5.12@gmail.com>
2019-09-24 17:58:29 +0800

    1. about  how to show router-view

---
+++ src/main/java/com/example/vuerouter/vuerouter/VuerouterApplication.java
@@ -0,0 +1,13 @@
+package com.example.vuerouter.vuerouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuerouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VuerouterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vuerouter/vuerouter/VuerouterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.vuerouter.vuerouter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VuerouterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
